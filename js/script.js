5c96549951a19bb55d2f12b4bd101882d397e559
Rajat Biswas <32998741+rajatdiptabiswas@users.noreply.github.com>
2018-07-02 01:55:56 +0600

    Add new functionality
    Added a backspace button.
    Allow stacked operations.
    Added new themes.
    Renamed old themes.
    Minor visual tweaks.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,12 @@ $(document).ready(function () {
     });
     
     // change theme
+    // default
+    $('#theme-purple').click(function () {
+        $('#main').css({"background": "linear-gradient(to bottom right, #FC3366, #3A5EFF"});
+        $('.op').css({"color": "lightslategray"});
+        $('.num-button').css({"color": "lightslategray"});
+    });
     // red
     $('#theme-red').click(function () {
         $('#main').css({"background": "linear-gradient(to bottom right, red, yellow"});
@@ -23,72 +29,134 @@ $(document).ready(function () {
         $('.op').css({"color": "dodgerblue"});
         $('.num-button').css({"color": "dodgerblue"});
     });
+    // blue green
+    $('#theme-bluegreen').click(function () {
+        $('#main').css({"background": "linear-gradient(to bottom, #5433ff, #20bdff, #a5ffaa)"});
+        $('.op').css({"color": "lightseagreen"});
+        $('.num-button').css({"color": "lightseagreen"});
+    });
     // green
     $('#theme-green').click(function () {
         $('#main').css({"background": "linear-gradient(to bottom right, mediumseagreen, springgreen"});
         $('.op').css({"color": "limegreen"});
         $('.num-button').css({"color": "limegreen"});
     });
+    // black
+    $('#theme-black').click(function () {
+        $('#main').css({"background": "linear-gradient(to bottom right, black, #434343"});
+        $('.op').css({"color": "#434343"});
+        $('.num-button').css({"color": "#434343"});
+    });
 });
 
-var siteWidth = 480;
-var scale = screen.width /siteWidth
+// prevents double tap to zoom
+var doubleTouchStartTimestamp = 0;
+$(document).bind("touchstart", function(event){
+    var now = +(new Date());
+    if (doubleTouchStartTimestamp + 500 > now){
+        event.preventDefault();
+    };
+    doubleTouchStartTimestamp = now;
+});
+
+// manages scaling
+let siteWidth = 480;
+let scale = screen.width /siteWidth
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
-var dec = false;
-var operator = false;
-var ansFound = false;
-var accuracy = 2;
+var dec = false;  // tracks if decimal has been entered
+var operator = false;  // tracks if an operator has been entered 
+var ansFound = false;  // tracks if ans has been calculated
+var accuracy = 2;  // determines the number of decimal places to show
+var ans = 0;  // the result
 
+// automatically scrolls to the rightmost side of the result window
 function autoScroll() {
     document.getElementById("result").scrollLeft = document.getElementById("result").scrollWidth;
 }
 
+// changes the number of decimal places to show
 function changePrecision(val) {
     accuracy = val;
 }
 
+// insert a value to the result window
 function insert(value) {
+    // if last pressed button was equals
     if (ansFound) {
-        c();
+        // only allow operators
+        if (value == '+' || value == '−' || value == '*' || value == '/') {
+            document.getElementById("result").innerHTML += value;
+            dec = false;
+            ansFound = false;
+        } else {
+            return;
+        }
     }
+    // allow only one decimal point per number
     if (value == '.') {
         if (dec || document.getElementById("result").innerHTML== '') {
             return;
         } else {
             dec = true; 
             document.getElementById("result").innerHTML += value;
-            autoScroll();
         }
-    } else if (value == '+' || value == '-' || value == '*' || value == '/') {
+    // allow only one operator to be on the result window for every two numbers
+    } else if (value == '+' || value == '−' || value == '*' || value == '/') {
         if (operator || document.getElementById("result").innerHTML== '') {
             return;
         } else {
             operator = true;
             document.getElementById("result").innerHTML += value;
             dec = false;
-            autoScroll();
         }
     } else {
         document.getElementById("result").innerHTML += value;
-        autoScroll();
+    }
+    
+    autoScroll();
+}
+
+// removes the last character
+function backspace() {
+    let string = document.getElementById("result").innerHTML + '';
+    let lastChar = string.substr(string.length - 1);
+    
+    if (string) {
+        if (lastChar == '.') {
+            // allow decimal if the decimal point is deleted
+            dec = false;
+        } else if (lastChar == '+' || lastChar == '−' || lastChar == '*' || lastChar == '/') {
+            // allow a operator if any one of them is deleted
+            operator = false;
+        }
+        
+        document.getElementById("result").innerHTML = string.substr(0, string.length - 1);
+        
+        // reset the calculator if backspace clears the result window
+        if (document.getElementById("result").innerHTML == "") {
+            c();
+        }
     }
 }
 
+// resets the calculator
 function c() {
     dec = false;
     operator = false;
     ansFound = false;
+    ans = 0;
     document.getElementById("result").innerHTML = "";
 }
 
+// evaluates the answer using the string in the result window
 function findAns() {
-    var ans = 0;
-    var str = document.getElementById("result").innerHTML + '';
+    let str = document.getElementById("result").innerHTML + '';
+    str = str.replace('−', '-'); // change MINUS SIGN “−” U+2212 with HYPHEN-MINUS, “-”, U+002D 
     
     if (str) {
-        ans = eval(document.getElementById("result").innerHTML + '');
+        ans = eval(str);
     }
     
     document.getElementById("result").innerHTML = parseFloat(ans).toFixed(accuracy);
